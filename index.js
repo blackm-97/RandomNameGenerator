c8af369e7d370fdfad3c9ac263781858e1d77083
blackm-97 <blackm97@msu.edu>
2025-02-19 20:13:40 -0500

    All code logic

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add JavaScript below
+let names = ['Adam', 'Reighleigh', 'Ashton', 'Jeff', 'Humphrey Sr. III', 'Emily', 'Athena', 'Herbert', 'Audrey', 'Wireless Dyson Vacuum Cleaner V2.4 with rechargable battery']
+
+function RandomNumber(max){
+    return Math.floor(Math.random() * max)
+}
+
+function GetRandomName(){
+    let num = RandomNumber(10)
+    return names[num];
+}
